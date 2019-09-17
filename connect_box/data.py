774b831ba2912d337221615392829e0145a402ca
Pascal Vizeli <pvizeli@syshack.ch>
2019-09-17 11:53:04 +0200

    Extend error handling (#9)

---
+++ connect_box/data.py
@@ -0,0 +1,48 @@
+"""Handle Data attributes."""
+from ipaddress import IPv4Address, IPv6Address, ip_address as convert_ip
+from typing import Union
+
+import attr
+
+
+@attr.s
+class Device:
+    """A single device."""
+
+    mac: str = attr.ib()
+    hostname: str = attr.ib(cmp=False)
+    ip: Union[IPv4Address, IPv6Address] = attr.ib(cmp=False, convert=convert_ip)
+
+
+@attr.s
+class DownstreamChannel:
+    """A locked downstream channel."""
+
+    frequency: int = attr.ib()
+    powerLevel: int = attr.ib()
+    modulation: str = attr.ib()
+    id: str = attr.ib()
+    snr: float = attr.ib()
+    preRs: int = attr.ib()
+    postRs: int = attr.ib()
+    qamLocked: bool = attr.ib()
+    fecLocked: bool = attr.ib()
+    mpegLocked: bool = attr.ib()
+
+
+@attr.s
+class UpstreamChannel:
+    """A locked upstream channel."""
+
+    frequency: int = attr.ib()
+    powerLevel: int = attr.ib()
+    symbolRate: str = attr.ib()
+    id: str = attr.ib()
+    modulation: str = attr.ib()
+    type: str = attr.ib()
+    t1Timeouts: int = attr.ib()
+    t2Timeouts: int = attr.ib()
+    t3Timeouts: int = attr.ib()
+    t4Timeouts: int = attr.ib()
+    channelType: str = attr.ib()
+    messageType: int = attr.ib()
