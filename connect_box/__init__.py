5bc53ae62595f6a4ff466cf486df4abfbb954058
Kiskae <Kiskae@users.noreply.github.com>
2021-06-29 03:43:33 +0000

    run formatter, isort

--- connect_box/__init__.py
+++ connect_box/__init__.py
@@ -1,54 +1,50 @@
 """A Python Client to get data from UPC Connect Boxes."""
 import asyncio
+from collections import OrderedDict
 import logging
 from typing import Dict, List, Optional
-from collections import OrderedDict
 
-import attr
 import aiohttp
 from aiohttp.hdrs import REFERER, USER_AGENT
 import defusedxml.ElementTree as element_tree
 
-from .parsers import _parse_general_time, _parse_daily_time
-
+from . import exceptions
 from .data import (
+    CmStatus,
     CmSystemInfo,
-    LanStatus,
-    WanStatus,
     Device,
     DownstreamChannel,
-    UpstreamChannel,
-    CmStatus,
+    FilterState,
+    FilterStatesList,
+    FiltersTimeMode,
+    Ipv6FilterInstance,
+    LanStatus,
+    LogEvent,
     ServiceFlow,
     Temperature,
-    Ipv6FilterInstance, 
-    FilterState, 
-    FilterStatesList, 
-    FiltersTimeMode,
-    LogEvent,
+    UpstreamChannel,
+    WanStatus,
 )
-
-from . import exceptions
-
+from .parsers import _parse_daily_time, _parse_general_time
 
 _LOGGER = logging.getLogger(__name__)
 
 HTTP_HEADER_X_REQUESTED_WITH = "X-Requested-With"
 
-CMD_LOGIN = 15
-CMD_LOGOUT = 16
-CMD_DEVICES = 123
-CMD_DOWNSTREAM = 10
-CMD_UPSTREAM = 11
-CMD_GET_IPV6_FILTER_RULE = 111
-CMD_SET_IPV6_FILTER_RULE = 112
-CMD_TEMPERATURE = 136
-CMD_CMSTATUS = 144
-CMD_EVENTLOG = 13
 CMD_CMSYSTEMINFO = 2
 CMD_REBOOT = 8
+CMD_DOWNSTREAM = 10
+CMD_UPSTREAM = 11
+CMD_EVENTLOG = 13
+CMD_LOGIN = 15
+CMD_LOGOUT = 16
 CMD_LANSTATUS = 100
 CMD_WANSTATUS = 107
+CMD_GET_IPV6_FILTER_RULE = 111
+CMD_SET_IPV6_FILTER_RULE = 112
+CMD_DEVICES = 123
+CMD_TEMPERATURE = 136
+CMD_CMSTATUS = 144
 
 
 class ConnectBox:
@@ -236,7 +232,7 @@ class ConnectBox:
             self._ipv6_filters_time = FiltersTimeMode(
                 int(xml_root.find("time_mode").text),
                 _parse_general_time(xml_root),
-                _parse_daily_time(xml_root)
+                _parse_daily_time(xml_root),
             )
 
         except (element_tree.ParseError, TypeError):
@@ -247,43 +243,48 @@ class ConnectBox:
     async def _async_get_ipv6_filter_states(self) -> FilterStatesList:
         """Get enable/disable states of IPv6 filter instances"""
         await self.async_get_ipv6_filtering()
-        return FilterStatesList(list(FilterState(filter_instance.idd, filter_instance.enabled) for filter_instance in self.ipv6_filters))
+        return FilterStatesList(
+            list(
+                FilterState(filter_instance.idd, filter_instance.enabled)
+                for filter_instance in self.ipv6_filters
+            )
+        )
 
     async def _async_update_ipv6_filter_states(self, filter_states: FilterStatesList):
         """Update enable/disable states of IPv6 filters while not affecting any other setting"""
         if self.token is None:
             await self.async_initialize_token()
 
-        val_enabled = '*'.join([str(fs.enabled) for fs in filter_states.entries])
-        val_del = '*'.join(['0' for fs in filter_states.entries])
-        val_idd = '*'.join([str(fs.idd) for fs in filter_states.entries])
+        val_enabled = "*".join([str(fs.enabled) for fs in filter_states.entries])
+        val_del = "*".join(["0" for fs in filter_states.entries])
+        val_idd = "*".join([str(fs.idd) for fs in filter_states.entries])
 
         params = OrderedDict()
-        params['act'] = 1
-        params['dir'] = 0
-        params['enabled'] = val_enabled
-        params['allow_traffic'] = ''
-        params['protocol'] = ''
-        params['src_addr'] = ''
-        params['src_prefix'] = ''
-        params['dst_addr'] = ''
-        params['dst_prefix'] = ''
-        params['ssport'] = ''
-        params['seport'] = ''
-        params['dsport'] = ''
-        params['deport'] = ''
-        params['del'] = val_del
-        params['idd'] = val_idd
-        params['sIpRange'] = ''
-        params['dsIpRange'] = ''
-        params['PortRange'] = ''
-        params['TMode'] = self._ipv6_filters_time.TMode
+        params["act"] = 1
+        params["dir"] = 0
+        params["enabled"] = val_enabled
+        params["allow_traffic"] = ""
+        params["protocol"] = ""
+        params["src_addr"] = ""
+        params["src_prefix"] = ""
+        params["dst_addr"] = ""
+        params["dst_prefix"] = ""
+        params["ssport"] = ""
+        params["seport"] = ""
+        params["dsport"] = ""
+        params["deport"] = ""
+        params["del"] = val_del
+        params["idd"] = val_idd
+        params["sIpRange"] = ""
+        params["dsIpRange"] = ""
+        params["PortRange"] = ""
+        params["TMode"] = self._ipv6_filters_time.TMode
         if self._ipv6_filters_time.TMode == 1:
-            params['TRule'] = self._ipv6_filters_time.XmlGeneralTime
+            params["TRule"] = self._ipv6_filters_time.XmlGeneralTime
         elif self._ipv6_filters_time.TMode == 2:
-            params['TRule'] = self._ipv6_filters_time.XmlDailyTime
+            params["TRule"] = self._ipv6_filters_time.XmlDailyTime
         else:
-            params['TRule'] = 0
+            params["TRule"] = 0
 
         await self._async_ws_set_function(CMD_SET_IPV6_FILTER_RULE, params)
 
@@ -438,10 +439,10 @@ class ConnectBox:
             xml_root = element_tree.fromstring(raw)
             for elmt_log_event in xml_root.iter("eventlog"):
                 log_event = LogEvent(
-                elmt_log_event.find("prior").text,
-                elmt_log_event.find("text").text,
-                elmt_log_event.find("time").text,
-                int(elmt_log_event.find("t").text),
+                    elmt_log_event.find("prior").text,
+                    elmt_log_event.find("text").text,
+                    elmt_log_event.find("time").text,
+                    int(elmt_log_event.find("t").text),
                 )
                 self.eventlog.append(log_event)
         except (element_tree.ParseError, TypeError):
@@ -536,7 +537,9 @@ class ConnectBox:
 
         raise exceptions.ConnectBoxConnectionError()
 
-    async def _async_ws_set_function(self, function: int, params: dict) -> Optional[bool]:
+    async def _async_ws_set_function(
+        self, function: int, params: dict
+    ) -> Optional[bool]:
         """Execute a set command on UPC firmware webservice.
 
         Args:
@@ -546,12 +549,12 @@ class ConnectBox:
         try:
             # The 'token' parameter has to be first, and 'fun' second
             # or the UPC firmware will return an error
-            params_str = ''.join([f'&{key}={value}' for (key, value) in params.items()])
+            params_str = "".join([f"&{key}={value}" for (key, value) in params.items()])
 
             async with await self._session.post(
                 f"http://{self.host}/xml/setter.xml",
                 data=f"token={self.token}&fun={function}{params_str}",
-                #data=params,
+                # data=params,
                 headers=self.headers,
                 allow_redirects=False,
                 timeout=10,
