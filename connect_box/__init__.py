4188ac05df3626f012bff5dfad6b800fb7cf7bde
Kiskae <Kiskae@users.noreply.github.com>
2022-06-05 18:03:22 +0200

    Add CmSystemInfo/WanStatus/LanStatus apis (#27)
    * Add CmSystemInfo api
    
    * Add LanStatus api
    
    * Add WanStatus api
    
    * Add api to start a device reboot
    
    * handle value refreshing consistently
    
    * run formatter, isort
    
    * Add api for global device settings

--- connect_box/__init__.py
+++ connect_box/__init__.py
@@ -1,47 +1,52 @@
 """A Python Client to get data from UPC Connect Boxes."""
 import asyncio
+from collections import OrderedDict
 import logging
 from typing import Dict, List, Optional
-from collections import OrderedDict
 
-import attr
 import aiohttp
 from aiohttp.hdrs import REFERER, USER_AGENT
 import defusedxml.ElementTree as element_tree
 
-from .parsers import _parse_general_time, _parse_daily_time
-
+from . import exceptions
 from .data import (
+    CmStatus,
+    CmSystemInfo,
     Device,
     DownstreamChannel,
-    UpstreamChannel,
-    CmStatus,
+    FilterState,
+    FilterStatesList,
+    FiltersTimeMode,
+    GlobalSettings,
+    Ipv6FilterInstance,
+    LanStatus,
+    LogEvent,
     ServiceFlow,
     Temperature,
-    Ipv6FilterInstance, 
-    FilterState, 
-    FilterStatesList, 
-    FiltersTimeMode,
-    LogEvent,
+    UpstreamChannel,
+    WanStatus,
 )
-
-from . import exceptions
-
+from .parsers import _parse_daily_time, _parse_general_time
 
 _LOGGER = logging.getLogger(__name__)
 
 HTTP_HEADER_X_REQUESTED_WITH = "X-Requested-With"
 
-CMD_LOGIN = 15
-CMD_LOGOUT = 16
-CMD_DEVICES = 123
+CMD_GLOBALSETTINGS = 1
+CMD_CMSYSTEMINFO = 2
+CMD_REBOOT = 8
 CMD_DOWNSTREAM = 10
 CMD_UPSTREAM = 11
+CMD_EVENTLOG = 13
+CMD_LOGIN = 15
+CMD_LOGOUT = 16
+CMD_LANSTATUS = 100
+CMD_WANSTATUS = 107
 CMD_GET_IPV6_FILTER_RULE = 111
 CMD_SET_IPV6_FILTER_RULE = 112
+CMD_DEVICES = 123
 CMD_TEMPERATURE = 136
 CMD_CMSTATUS = 144
-CMD_EVENTLOG = 13
 
 
 class ConnectBox:
@@ -73,13 +78,18 @@ class ConnectBox:
         self.upstream_service_flows: List[ServiceFlow] = []
         self.downstream_service_flows: List[ServiceFlow] = []
         self.temperature: Optional[Temperature] = None
+        self.eventlog: List[LogEvent] = []
+        self.lanstatus: Optional[LanStatus] = None
+        self.wanstatus: Optional[WanStatus] = None
+        self.cm_systeminfo: Optional[CmSystemInfo] = None
+        self.global_settings: Optional[GlobalSettings] = None
 
     async def async_get_devices(self):
         """Scan for new devices and return a list with found device IDs."""
         if self.token is None:
             await self.async_initialize_token()
 
-        self.devices.clear()
+        self.devices = []
         raw = await self._async_ws_get_function(CMD_DEVICES)
 
         try:
@@ -136,7 +146,7 @@ class ConnectBox:
         if self.token is None:
             await self.async_initialize_token()
 
-        self.ds_channels.clear()
+        self.ds_channels = []
         raw = await self._async_ws_get_function(CMD_DOWNSTREAM)
 
         try:
@@ -166,7 +176,7 @@ class ConnectBox:
         if self.token is None:
             await self.async_initialize_token()
 
-        self.us_channels.clear()
+        self.us_channels = []
         raw = await self._async_ws_get_function(CMD_UPSTREAM)
 
         try:
@@ -198,7 +208,7 @@ class ConnectBox:
         if self.token is None:
             await self.async_initialize_token()
 
-        self.ipv6_filters.clear()
+        self.ipv6_filters = []
         self._ipv6_filters_time = None
         raw = await self._async_ws_get_function(CMD_GET_IPV6_FILTER_RULE)
 
@@ -225,7 +235,7 @@ class ConnectBox:
             self._ipv6_filters_time = FiltersTimeMode(
                 int(xml_root.find("time_mode").text),
                 _parse_general_time(xml_root),
-                _parse_daily_time(xml_root)
+                _parse_daily_time(xml_root),
             )
 
         except (element_tree.ParseError, TypeError):
@@ -236,43 +246,48 @@ class ConnectBox:
     async def _async_get_ipv6_filter_states(self) -> FilterStatesList:
         """Get enable/disable states of IPv6 filter instances"""
         await self.async_get_ipv6_filtering()
-        return FilterStatesList(list(FilterState(filter_instance.idd, filter_instance.enabled) for filter_instance in self.ipv6_filters))
+        return FilterStatesList(
+            list(
+                FilterState(filter_instance.idd, filter_instance.enabled)
+                for filter_instance in self.ipv6_filters
+            )
+        )
 
     async def _async_update_ipv6_filter_states(self, filter_states: FilterStatesList):
         """Update enable/disable states of IPv6 filters while not affecting any other setting"""
         if self.token is None:
             await self.async_initialize_token()
 
-        val_enabled = '*'.join([str(fs.enabled) for fs in filter_states.entries])
-        val_del = '*'.join(['0' for fs in filter_states.entries])
-        val_idd = '*'.join([str(fs.idd) for fs in filter_states.entries])
+        val_enabled = "*".join([str(fs.enabled) for fs in filter_states.entries])
+        val_del = "*".join(["0" for fs in filter_states.entries])
+        val_idd = "*".join([str(fs.idd) for fs in filter_states.entries])
 
         params = OrderedDict()
-        params['act'] = 1
-        params['dir'] = 0
-        params['enabled'] = val_enabled
-        params['allow_traffic'] = ''
-        params['protocol'] = ''
-        params['src_addr'] = ''
-        params['src_prefix'] = ''
-        params['dst_addr'] = ''
-        params['dst_prefix'] = ''
-        params['ssport'] = ''
-        params['seport'] = ''
-        params['dsport'] = ''
-        params['deport'] = ''
-        params['del'] = val_del
-        params['idd'] = val_idd
-        params['sIpRange'] = ''
-        params['dsIpRange'] = ''
-        params['PortRange'] = ''
-        params['TMode'] = self._ipv6_filters_time.TMode
+        params["act"] = 1
+        params["dir"] = 0
+        params["enabled"] = val_enabled
+        params["allow_traffic"] = ""
+        params["protocol"] = ""
+        params["src_addr"] = ""
+        params["src_prefix"] = ""
+        params["dst_addr"] = ""
+        params["dst_prefix"] = ""
+        params["ssport"] = ""
+        params["seport"] = ""
+        params["dsport"] = ""
+        params["deport"] = ""
+        params["del"] = val_del
+        params["idd"] = val_idd
+        params["sIpRange"] = ""
+        params["dsIpRange"] = ""
+        params["PortRange"] = ""
+        params["TMode"] = self._ipv6_filters_time.TMode
         if self._ipv6_filters_time.TMode == 1:
-            params['TRule'] = self._ipv6_filters_time.XmlGeneralTime
+            params["TRule"] = self._ipv6_filters_time.XmlGeneralTime
         elif self._ipv6_filters_time.TMode == 2:
-            params['TRule'] = self._ipv6_filters_time.XmlDailyTime
+            params["TRule"] = self._ipv6_filters_time.XmlDailyTime
         else:
-            params['TRule'] = 0
+            params["TRule"] = 0
 
         await self._async_ws_set_function(CMD_SET_IPV6_FILTER_RULE, params)
 
@@ -292,6 +307,62 @@ class ConnectBox:
 
         _LOGGER.warning("Filter %d not found", idd)
         return None
+
+    async def async_get_lanstatus(self):
+        """Access information related to the router gateway"""
+        if self.token is None:
+            await self.async_initialize_token()
+
+        self.lanstatus = None
+        raw = await self._async_ws_get_function(CMD_LANSTATUS)
+        try:
+            xml_root = element_tree.fromstring(raw)
+            self.lanstatus = LanStatus(
+                upnp_enabled=xml_root.find("UPnP").text == "1",
+                mac=xml_root.find("LanMAC").text,
+                ip4=xml_root.find("LanIP").text,
+                ip6=xml_root.find("LanIPv6").text,
+            )
+        except (element_tree.ParseError, TypeError):
+            _LOGGER.warning("Can't read lanstatus from %s", self.host)
+            self.token = None
+            raise exceptions.ConnectBoxNoDataAvailable() from None
+
+    async def async_get_wanstatus(self):
+        """Access information related to the WAN port"""
+        if self.token is None:
+            await self.async_initialize_token()
+
+        self.wanstatus = None
+        raw = await self._async_ws_get_function(CMD_WANSTATUS)
+        try:
+            xml_root = element_tree.fromstring(raw)
+            self.wanstatus = WanStatus(
+                mac=xml_root.find("WanMAC").text, ip4=xml_root.find("WanIP").text
+            )
+        except (element_tree.ParseError, TypeError):
+            _LOGGER.warning("Can't read wanstatus from %s", self.host)
+            self.token = None
+            raise exceptions.ConnectBoxNoDataAvailable() from None
+
+    async def async_get_cm_system_info(self):
+        """Access information related to the cable modem"""
+        if self.token is None:
+            await self.async_initialize_token()
+
+        self.cm_systeminfo = None
+        raw = await self._async_ws_get_function(CMD_CMSYSTEMINFO)
+        try:
+            xml_root = element_tree.fromstring(raw)
+            self.cm_systeminfo = CmSystemInfo(
+                mac=xml_root.find("cm_mac_addr").text,
+                serial=xml_root.find("cm_serial_number").text,
+                network_access=xml_root.find("cm_network_access").text == "Allowed",
+            )
+        except (element_tree.ParseError, TypeError):
+            _LOGGER.warning("Can't read cm system info from %s", self.host)
+            self.token = None
+            raise exceptions.ConnectBoxNoDataAvailable() from None
 
     async def async_get_cmstatus_and_service_flows(self):
         """Get various status information."""
@@ -374,10 +445,10 @@ class ConnectBox:
             xml_root = element_tree.fromstring(raw)
             for elmt_log_event in xml_root.iter("eventlog"):
                 log_event = LogEvent(
-                elmt_log_event.find("prior").text,
-                elmt_log_event.find("text").text,
-                elmt_log_event.find("time").text,
-                int(elmt_log_event.find("t").text),
+                    elmt_log_event.find("prior").text,
+                    elmt_log_event.find("text").text,
+                    elmt_log_event.find("time").text,
+                    int(elmt_log_event.find("t").text),
                 )
                 self.eventlog.append(log_event)
         except (element_tree.ParseError, TypeError):
@@ -385,6 +456,16 @@ class ConnectBox:
             self.token = None
             raise exceptions.ConnectBoxNoDataAvailable() from None
         self.eventlog.sort(key=(lambda e: e.evEpoch))
+
+    async def async_reboot_device(self) -> None:
+        """Request that the device reboot"""
+        if self.token is None:
+            await self.async_initialize_token()
+
+        await self._async_ws_set_function(CMD_REBOOT, params={})
+
+        # At this point the device must be restarting, so the token becomes invalid
+        self.token = None
 
     async def async_close_session(self) -> None:
         """Logout and close session."""
@@ -394,8 +475,37 @@ class ConnectBox:
         await self._async_ws_set_function(CMD_LOGOUT, {})
         self.token = None
 
+    async def async_get_global_settings(self) -> None:
+        """Access the global settings, reduced information is available if not logged in before."""
+        if self.token is None:
+            # login is not required for this method
+            await self._async_initialize_valid_token()
+
+        self.global_settings = None
+        raw = await self._async_ws_get_function(CMD_GLOBALSETTINGS)
+        try:
+            xml_root = element_tree.fromstring(raw)
+            sw_version = xml_root.find("SwVersion")
+            global_settings = GlobalSettings(
+                logged_in=xml_root.find("AccessLevel").text == "1",
+                operator_id=xml_root.find("OperatorId").text,
+                # if logged in or nobody is logged in, this returns NONE
+                access_denied=xml_root.find("AccessDenied").text != "NONE",
+                sw_version=sw_version.text if sw_version else None,
+            )
+            self.global_settings = global_settings
+        except (element_tree.ParseError, TypeError):
+            _LOGGER.warning("Can't read global settings from %s", self.host)
+            self.token = None
+            raise exceptions.ConnectBoxNoDataAvailable() from None
+
     async def async_initialize_token(self) -> None:
         """Get the token first."""
+        await self._async_initialize_valid_token()
+        await self._async_do_login_with_password(CMD_LOGIN)
+
+    async def _async_initialize_valid_token(self) -> None:
+        """Initialize self.token to a known good value"""
         try:
             # Get first the token
             async with self._session.get(
@@ -410,9 +520,7 @@ class ConnectBox:
             _LOGGER.error("Can not load login page from %s: %s", self.host, err)
             raise exceptions.ConnectBoxConnectionError()
 
-        await self._async_initialize_token_with_password(CMD_LOGIN)
-
-    async def _async_initialize_token_with_password(self, function: int) -> None:
+    async def _async_do_login_with_password(self, function: int) -> None:
         """Get token with password."""
         try:
             async with await self._session.post(
@@ -463,7 +571,9 @@ class ConnectBox:
 
         raise exceptions.ConnectBoxConnectionError()
 
-    async def _async_ws_set_function(self, function: int, params: dict) -> Optional[bool]:
+    async def _async_ws_set_function(
+        self, function: int, params: dict
+    ) -> Optional[bool]:
         """Execute a set command on UPC firmware webservice.
 
         Args:
@@ -473,12 +583,12 @@ class ConnectBox:
         try:
             # The 'token' parameter has to be first, and 'fun' second
             # or the UPC firmware will return an error
-            params_str = ''.join([f'&{key}={value}' for (key, value) in params.items()])
+            params_str = "".join([f"&{key}={value}" for (key, value) in params.items()])
 
             async with await self._session.post(
                 f"http://{self.host}/xml/setter.xml",
                 data=f"token={self.token}&fun={function}{params_str}",
-                #data=params,
+                # data=params,
                 headers=self.headers,
                 allow_redirects=False,
                 timeout=10,
